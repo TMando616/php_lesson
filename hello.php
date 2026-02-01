364a4b3f1c87606a312af16023ddc1a2d4055718
TMando616 <t.mandokoro616@gmail.com>
2026-02-01 12:13:45 +0900

    実践的な例

--- hello.php
+++ hello.php
@@ -1,6 +1,6 @@
 <?php
 
-    echo "Hello World";
+    // echo "Hello World";
 
     // php -S localhost:8000
     // 上記コマンドでサーバー立ち上げ
@@ -8,4 +8,24 @@
     
     // phpinfo();
 
+    echo $_SERVER['HTTP_USER_AGENT']
+
+?>
+
+<?php
+if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
+?>
+
+<h3>str_contains() がtrueを返しました</h3>
+<center>あなたはChromeを使用しています</center>
+
+<?php
+} else {
+?>
+
+<h3>str_contains()がfalseを返しました</h3>
+<center>あなたはChromeを使用していません</center>
+
+<?php
+}
 ?>
